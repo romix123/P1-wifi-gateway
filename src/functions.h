a57f3849c84d9a98e35c06e589c8d92c87c19674
Ronnie van den Crommenacker <ronnie@owello.nl>
2024-02-21 20:57:38 +0100

    Removed duplicate blink function

--- src/functions.h
+++ src/functions.h
@@ -44,14 +44,6 @@ void alignToTelegram() {
   }
 }
 
-void blink(int t) {
-  for (int i = 0; i <= t; i++) {
-    LEDon           // Signaal naar laag op ESP-M3
-        delay(200); // wacht 200 millisec
-    LEDoff;         // LEDoff, signaal naar hoog op de ESP-M3
-  }
-}
-
 void RTS_on() {           // switch on Data Request
   digitalWrite(OE, LOW);  // enable buffer
   digitalWrite(DR, HIGH); // turn on Data Request
